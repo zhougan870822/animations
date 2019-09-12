a8ee800812b6c8887963de49f293383b7edb32bf
zhougan <564169392@qq.com>
2019-09-12 14:28:57 +0800

    添加一些动画

--- app/src/main/java/com/zhoug/animations/module/MainActivity.java
+++ app/src/main/java/com/zhoug/animations/module/MainActivity.java
@@ -1,13 +1,39 @@
 package com.zhoug.animations.module;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private View bottomView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomView=findViewById(R.id.ll_bottom);
+
+        addListener();
+    }
+
+    private void addListener(){
+
+        findViewById(R.id.btn1).setOnClickListener(v->{
+            if(bottomView.getVisibility()==View.GONE){
+                bottomView.setVisibility(View.VISIBLE);
+            }else{
+                bottomView.setVisibility(View.GONE);
+            }
+
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(v->{
+            Intent intent=new Intent(this,NextActivity.class);
+
+            startActivity(intent);
+
+        });
+
     }
 }

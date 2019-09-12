13e536cae6277dfdb2e2f7905dc46edb2c72c69f
zhougan <564169392@qq.com>
2019-09-12 18:03:17 +0800

    添加一些动画3

--- app/src/main/java/com/zhoug/animations/module/NextActivity.java
+++ app/src/main/java/com/zhoug/animations/module/NextActivity.java
@@ -1,7 +1,9 @@
 package com.zhoug.animations.module;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(this, Next2Activity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.anim_popu_in,R.anim.anim_popu_out_right );
+
     }
 }
